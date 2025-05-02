adf0ff839f9cb03c55ea3846a83daecf250e2454
SagarDussa <sagar@abssoftware.in>
2025-05-02 16:34:55 +0530

    add netlify toml

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { Quiz2Component } from './components/quiz2/quiz2.component';
 const routes: Routes = [
   { path: 'mock1', component: QuizComponent },
   { path: 'mock2', component: Quiz2Component },
-  { path: '', redirectTo: '/mock1', pathMatch: 'full' }, // default route
+  { path: ' ', redirectTo: '/mock1', pathMatch: 'full' }, // default route
   { path: '**', redirectTo: '/mock1' } // wildcard redirect
 ];
 
